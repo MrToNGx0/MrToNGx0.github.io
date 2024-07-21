bd7215c9ceda118ba8e929d94b7e69b5321559f5
MrToNGx0 <tongsukkha@gmail.com>
2024-07-21 22:45:21 +0700

    add config api endpoint

--- src/app/core/config/config.ts
+++ src/app/core/config/config.ts
@@ -1,3 +1,11 @@
 export const config = {
-  production: false,
+  api : {
+    endpoint: {
+      profilePath: 'assets/json/profile.json',
+      skillPath: 'assets/json/skill.json',
+      certificatePath: 'assets/json/certificate.json',
+      experiencePath: 'assets/json/experience.json',
+      educationPath: 'assets/json/education.json'
+    }
+  }
 };
