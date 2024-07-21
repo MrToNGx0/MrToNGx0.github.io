3088f5c56ec25b0b426ed4c70c4fa4f58f5126b6
MrToNGx0 <tongsukkha@gmail.com>
2024-07-21 23:04:30 +0700

    change switch toggle dark mode

--- src/app/shared/component/navbar/navbar.component.ts
+++ src/app/shared/component/navbar/navbar.component.ts
@@ -14,6 +14,7 @@ import { TranslateService } from '@ngx-translate/core';
 export class NavbarComponent implements OnInit {
   listMenu!: ListMenu[];
   language!: Language[];
+  isDarkMode!: boolean;
   isSelectLanguage!: Language;
 
   faSun = faCloudSun;
@@ -31,6 +32,7 @@ export class NavbarComponent implements OnInit {
   }
 
   ngOnInit(): void {
+    this.isDarkMode = this.themeService.isDarkMode()
     this.language = this.languageService.getLanguage();
     this.languageService.getSelectedLanguage().subscribe(language => {
       if (language) {
